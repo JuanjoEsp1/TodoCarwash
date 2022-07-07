5df1cc0321eccc565ab66a8e503a8128f9eba91a
JuanjoEsp1 <espinozajuanjo26@gmail.com>
2022-07-07 00:11:27 -0400

    a

--- Funciones/RegistrarFunc.php
+++ Funciones/RegistrarFunc.php
@@ -11,8 +11,8 @@
 
 <body>
         <?php
-        $conexion = mysqli_connect("localhost", "root", "", "todocarwash2") or
-                die("Problemas con la conexion");
+        include('../Funciones/db.php');
+        
         mysqli_query($conexion, "insert into empresa(rut_empresa,nombre_empresa,calle,numeracion,comuna,telefono_empresa,correo_empresa,contrasena) values ('$_REQUEST[rut_empresa]','$_REQUEST[nombre_empresa]','$_REQUEST[calle]','$_REQUEST[numeracion]','$_REQUEST[comuna]','$_REQUEST[telefono_empresa]','$_REQUEST[correo_empresa]','$_REQUEST[contrasena]')")
 
                 or die("Problemas en la consulta" . mysqli_error($conexion));
